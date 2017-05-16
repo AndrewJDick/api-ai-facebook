b316cfee1bd76c7a001dd2a1637f6be059ccc139
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-05-16 16:54:44 +0100

    added mongodb capabilities

--- src/mongoDb.js
+++ src/mongoDb.js
@@ -1,70 +1,68 @@
-module.exports = () => {
+/*
+ * Copyright (c) 2017 ObjectLabs Corporation
+ * Distributed under the MIT license - http://opensource.org/licenses/MIT
+ *
+ * Written with: mongodb@2.2.21
+ * Documentation: http://docs.mongodb.org/ecosystem/drivers/node-js/
+ * A Node script connecting to a MongoDB database given a MongoDB Connection URI.
+*/
 
-  /*
-   * Copyright (c) 2017 ObjectLabs Corporation
-   * Distributed under the MIT license - http://opensource.org/licenses/MIT
-   *
-   * Written with: mongodb@2.2.21
-   * Documentation: http://docs.mongodb.org/ecosystem/drivers/node-js/
-   * A Node script connecting to a MongoDB database given a MongoDB Connection URI.
-  */
+console.log('accessed mongodb');
 
-  var mongodb = require('mongodb');
-  var uri = 'mongodb://admin:root@ds137441.mlab.com:37441/heroku_sxrcs6jm';
+var mongodb = require('mongodb');
+var uri = 'mongodb://admin:root@ds137441.mlab.com:37441/heroku_sxrcs6jm';
 
-  // Dummy data
-  var seedData = [
-    {
-      psid: '9999785059048814',
-      origin: '51.6564890, -0.3903200',
-      destination: '51.5238910,-0.0968820',
-      arrival: '09:00:00',
-      mode: 'transit',
-      preference: 'rail'
-    },
-    {
-      psid: '1709785059048999',
-      origin: '51.475579, -0.064370',
-      destination: '51.5238910,-0.0968820',
-      arrival: '11:00:00',
-      mode: 'driving',
-      preference: ''
-    },
-    {
-      psid: '9999995059048814',
-      origin: '51.059771, -1.310142',
-      destination: '51.5238910,-0.0968820',
-      arrival: '17:03:40',
-      mode: 'transit',
-      preference: 'bus'
-    },
-  ];
+// Dummy data
+var seedData = [
+  {
+    psid: '9999785059048814',
+    origin: '51.6564890, -0.3903200',
+    destination: '51.5238910,-0.0968820',
+    arrival: '09:00:00',
+    mode: 'transit',
+    preference: 'rail'
+  },
+  {
+    psid: '1709785059048999',
+    origin: '51.475579, -0.064370',
+    destination: '51.5238910,-0.0968820',
+    arrival: '11:00:00',
+    mode: 'driving',
+    preference: ''
+  },
+  {
+    psid: '9999995059048814',
+    origin: '51.059771, -1.310142',
+    destination: '51.5238910,-0.0968820',
+    arrival: '17:03:40',
+    mode: 'transit',
+    preference: 'bus'
+  },
+];
 
-  // DB Logic
-  mongodb.MongoClient.connect(uri, function(err, db) {
-    
-    if(err) throw err;
+// DB Logic
+mongodb.MongoClient.connect(uri, function(err, db) {
+  
+  if(err) throw err;
 
-    // DB Seed
-    db.listCollections().toArray(function(err, collections) {
-      var seeded = false;
+  // DB Seed
+  db.listCollections().toArray(function(err, collections) {
+    var seeded = false;
 
-      for (collection in collections) {
-        if (collections[collection].name === 'commutes') {
-          seeded = true;
-          break;
-        }
+    for (collection in collections) {
+      if (collections[collection].name === 'commutes') {
+        seeded = true;
+        break;
       }
-
-      if (!seeded) seedDb();
-    });
-
-    function seedDb() {
-
-      db.collection('commutes').insert(seedData, function(err, result) {
-        if(err) throw err;
-      });
     }
+
+    if (!seeded) seedDb();
   });
 
-}
+  function seedDb() {
+
+    db.collection('commutes').insert(seedData, function(err, result) {
+      if(err) throw err;
+    });
+  }
+});
