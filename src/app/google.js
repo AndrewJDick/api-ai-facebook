1d61a0cb55e1dfa4220d8b77424dbce0d6ebf149
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-05-16 22:51:07 +0100

    added user commute object for db storage

---
+++ src/app/google.js
@@ -0,0 +1,32 @@
+// Packages
+const nodeGeocoder = require('node-geocoder');
+
+// TODO: Replace apiKey with env variable
+const geocoderOptions = {
+  provider: 'google',
+ 
+  // Optional depending on the providers 
+  httpAdapter: 'https',
+  apiKey: 'AIzaSyDodAp8X1I7gbRdnvuv_0Pu-l6HQuGBJWE',
+  formatter: null
+};
+
+const geocoder = nodeGeocoder(geocoderOptions);
+
+const latLng = function(commuteContext) {
+	
+	const waypoints = [commuteContext.parameters.origin, commuteContext.parameters.destination];
+
+	for (waypoint in waypoints) {
+		geocoder.geocode(waypoints[waypoint]).then((response) => {
+	        
+			waypoints[waypoint] = `${response[0].latitude},${response[0].longitude}`;
+		    
+		})
+		.catch((err) => {
+		    console.log(err);
+		});
+	}
+};
+
+exports.latLng = latLng;
