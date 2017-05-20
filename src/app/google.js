9e8b4395b03bfcb7374d8a4b4a8e453932933f12
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-05-20 20:23:53 +0100

    added console debugging

--- src/app/google.js
+++ src/app/google.js
@@ -43,24 +43,26 @@ const latLng = (commuteContext, prop) => {
 };
 
 const addressToCoords = (commuteContext) => {
+	
 	console.log('at least were here');
-	let origin = new Promise((resolve, reject) => { 
-		console.log(latLng(commuteContext, 'origin'));
-		resolve(latLng(commuteContext, 'origin'));
-	}); 
+	
+	// let origin = new Promise((resolve, reject) => { 
+	// 	console.log(latLng(commuteContext, 'origin'));
+	// 	resolve(latLng(commuteContext, 'origin'));
+	// }); 
 
-	let destination = new Promise((resolve, reject) => { 
-		console.log(latLng(commuteContext, 'destination'));
-		resolve(fooBar(commuteContext, 'destination'));
-	});
+	// let destination = new Promise((resolve, reject) => { 
+	// 	console.log(latLng(commuteContext, 'destination'));
+	// 	resolve(fooBar(commuteContext, 'destination'));
+	// });
 
-    Promise.all([origin, destination])
-	    .then((values) => { 
-			console.log('values');
-			console.log(values);
-		}, (reason) => {
-			console.log(reason)
-		});
+ //    Promise.all([origin, destination])
+	//     .then((values) => { 
+	// 		console.log('values');
+	// 		console.log(values);
+	// 	}, (reason) => {
+	// 		console.log(reason)
+	// 	});
 
 };
 
