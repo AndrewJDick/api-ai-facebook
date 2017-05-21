4c8e6170cb66ba3f8cc22596032d834ba3d3b19f
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-05-21 20:13:54 +0100

    refactored seed db

--- src/app/mongo.js
+++ src/app/mongo.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2017 ObjectLabs Corporation
+ * Distributed under the MIT license - http://opensource.org/licenses/MIT
+ *
+ * Written with: mongodb@2.2.21
+ * Documentation: http://docs.mongodb.org/ecosystem/drivers/node-js/
+ * A Node script connecting to a MongoDB database given a MongoDB Connection URI.
+*/
+
 // Packages
 const mongodb = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
@@ -5,54 +14,61 @@ const assert = require('assert');
 const uri = process.env.MONGODB_URI;
 
 
-// Seed Database
-const seedDb = (db, callback) => {
-    
-    db.listCollections().toArray((err, collections) => {
-        let seeded = false;
-
-        for (collection in collections) {
-            if (collections[collection].name === 'commutes') {
-                seeded = true;
-                break;
-            }
-        }
-
-        // Seed the DB if the commutes collection does not exist
-        let isSeeded = (!seeded) ? insertSeed() : callback();
+// Dummy data
+const seedDb = (db, closeDb) => {
+    db.collection('commutes').insert([{
+        psid: '9999999999999999',
+        origin: '51.6564890,-0.3903200',
+        destination: '51.5238910,-0.0968820',
+        arrival: '09:00:00',
+        mode: 'transit',
+        preference: 'rail'
+    },
+    {
+        psid: '5555555555555555',
+        origin: '51.475579,-0.064370',
+        destination: '51.5238910,-0.0968820',
+        arrival: '11:00:00',
+        mode: 'driving',
+        preference: ''
+    },
+    {
+        psid: '4444444444444444',
+        origin: '51.059771,-1.310142',
+        destination: '51.5238910,-0.0968820',
+        arrival: '17:03:40',
+        mode: 'transit',
+        preference: 'bus'
+    }], (err, result) => {
+        assert.equal(err, null);
+        console.log("Inserted seed data into the commutes collection.");
+        closeDb();
     });
-
-    const insertSeed = () => {
-        db.collection('commutes').insert([{
-            psid: '9999999999999999',
-            origin: '51.6564890,-0.3903200',
-            destination: '51.5238910,-0.0968820',
-            arrival: '09:00:00',
-            mode: 'transit',
-            preference: 'rail'
-        },
-        {
-            psid: '5555555555555555',
-            origin: '51.475579,-0.064370',
-            destination: '51.5238910,-0.0968820',
-            arrival: '11:00:00',
-            mode: 'driving',
-            preference: ''
-        },
-        {
-            psid: '4444444444444444',
-            origin: '51.059771,-1.310142',
-            destination: '51.5238910,-0.0968820',
-            arrival: '17:03:40',
-            mode: 'transit',
-            preference: 'bus'
-        }], (err, result) => {
-            assert.equal(err, null);
-            console.log("Inserted seed data into the commutes collection.");
-            callback();
-        });
-    };
 };
+
+
+// DB Seed
+const isSeeded = (() => {
+    mongodb.connect(uri, (err, db) => {
+  
+        assert.equal(err, null);
+
+        db.listCollections().toArray((err, collections) => {
+            var seeded = false;
+
+            for (collection in collections) {
+                if (collections[collection].name === 'commutes') {
+                    seeded = true;
+                    break;
+                }
+            }
+
+            if (!seeded) seedDb(db, () => {
+                db.close();
+            });
+        });
+    });
+})();
 
 
 // Add or update a user's commute in the mongoDB commutes collection
@@ -89,8 +105,7 @@ const addCommute = (db, commute, callback) => {
 };
 
 
-// Establish a connection to the MongoDB Addon, then execute desired function
-const dbConnect = (method, commute = {}) => {
+const dbConnect = (commute, method) => {
     mongodb.connect(uri, (err, db) => {
         
         assert.equal(null, err);
@@ -101,21 +116,12 @@ const dbConnect = (method, commute = {}) => {
                     db.close();
                 })
                 break;
-            case 'seedDb':
-                seedDb(db, () => {
-                    db.close();
-                })
             default:
                 db.close();
                 break;
         }
     });
 };
-
-// Check for empty DB on deploy
-const onDeploy = (() => {
-    dbConnect('seedDb');
-})();
 
 
 // Exports 
